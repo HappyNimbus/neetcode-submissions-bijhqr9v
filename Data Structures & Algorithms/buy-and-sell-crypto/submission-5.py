19597ac38b438b248a42184b16bfec82fff0ef37
Maksim Kolotev <77296329+HappyNimbus@users.noreply.github.com>
2026-04-08 19:44:46 -0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        max_profit = 0
+        l = 0  
+
+        for r in range(len(prices)):
+            profit = prices[r] - prices[l]
+            max_profit = max(max_profit, profit)
+
+            if prices[r] <= prices[l]:
+                l = r
+
+
+        return max_profit        
